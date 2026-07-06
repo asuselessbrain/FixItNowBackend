541079ca2037cefa5dc871d9d538ee18086ad5d1
asuselessbrain <arfan18@cse.pstu.ac.bd>
2026-07-06 20:01:26 +0600

    Add: auth middleware

--- src/app/modules/auth/auth.router.ts
+++ src/app/modules/auth/auth.router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import auth from "../../../middlewares/auth";
+import { Role } from "../../../../prisma/generated/prisma/enums";
 
 const router = Router();
 
@@ -7,6 +9,6 @@ router.post("/login", authController.login)
 router.get('/refresh-token', authController.generateAccessTokenUsingRefreshToken);
 router.post('/forget-password', authController.forgetPassword);
 router.post('/reset-password', authController.resetPassword);
-router.post('/change-password', authController.changePassword);
+router.post('/change-password', auth(Role.admin, Role.customer, Role.technician), authController.changePassword);
 
 export const authRouter = router;
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,62 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./appError";
+import { jwtVerifier } from "../../lib/jwt";
+import { config } from "../../config";
+import { JwtPayload, Secret } from "jsonwebtoken";
+import { prisma } from "../../lib/prisma";
+import { Status } from "../../prisma/generated/prisma/enums";
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization;
+      
+      if (!token) {
+        throw new AppError(401, "Invalid signature");
+      }
+
+      const bearerToken = token.split(" ")[1];
+      let decoded;
+
+      try {
+        decoded = jwtVerifier({
+          token: bearerToken as string,
+          secretKey: config.jwt.token_secret as Secret,
+        }) as JwtPayload;
+      } catch (err: any) {
+        if (err.name === "TokenExpiredError") {
+          throw new AppError(401, "Access token expired");
+        }
+        if (err.name === "JsonWebTokenError") {
+          throw new AppError(401, "Invalid token");
+        }
+        throw new AppError(401, "Unauthorized");
+      }
+
+      console.log(decoded);
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: decoded.email,
+          status: Status.active,
+        },
+      });
+
+      if (!user) {
+        throw new AppError(404, "User not found");
+      }
+
+      if (roles.length && !roles.includes(user.role)) {
+        throw new AppError(403, "Unauthorized to access this resource");
+      }
+
+      req.user = decoded as JwtPayload;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default auth;
